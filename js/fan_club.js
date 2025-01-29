cc5c6a4c2ec0f6d240d8315874c612307b5be28c
kaley-cuoco-official <mail@avilez.com.mx>
2025-01-29 15:28:37 +0100

    Add files via upload

---
+++ js/fan_club.js
@@ -0,0 +1,5 @@
+// 
+function fanClubPage() {
+    let fanClub = `Loop random fans`;
+    $("#main_bar").html(fanClub)
+}
---
+++ js/meet_and_greet.js
@@ -0,0 +1,6 @@
+// 
+function meetAndGreetPage() {
+    
+    let meetGreet = `Ability to meet depends on ya plan. Above 1k. Days longivity depends on plan`
+    $("#main_bar").html(meetGreet)
+}
